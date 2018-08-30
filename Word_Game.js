3cf7b47e996f7321508587ef7f533fcfb55f506f
Pete Gallagher <petegallagher@Peter.local>
2018-08-30 08:19:02 -0700

    Added new files

---
+++ Word_Game.js
@@ -0,0 +1,51 @@
+var fs = require("fs");
+
+function logBefore(func, message) {
+  console.log(message);
+  func();
+}
+
+
+function runIf(func, bool) {
+  if (bool) {
+    func();
+  }
+}
+
+function wrap(func, value) {
+  return function() {
+    return func(value);
+  };
+}
+
+function chicken(word){
+  console.log(word)
+}
+
+var rooster = wrap(chicken, "David")
+rooster();
+
+// Callbacks appear in familiar places, like fs.writeFile.
+fs.writeFile("log.txt", "Log message!", function(err) {
+
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("File saved!");
+
+});
+
+// Alternatively, we can assign our function to a variable,
+// and pass it by name.
+var writeFileCallback = function(err) {
+
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("File saved!");
+
+};
+
+fs.writeFile("log.txt", "Log message!", writeFileCallback);
